c85fadef8361db9a48bb0a20e3daf46184faadac
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2023-04-18 20:47:37 +0200

    Init shared volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 set -ex
 
 umask 0000
-mkdir -p /tmp/.X11-unix
+rm -rf /mnt/wslg
+chown wslg:wslg /mnt/wslg
+mkdir -p /mnt/wslg/.X11-unix /mnt/wslg/runtime-dir /tmp/.X11-unix
 mount --bind /mnt/wslg/.X11-unix /tmp/.X11-unix
 socat -dd tcp-listen:3389,fork,reuseaddr unix-connect:/mnt/wslg/rdp.sock &
-rm /mnt/wslg/*.log
-#tail -F /mnt/wslg/{pulseaudio,stderr,weston,wlog}.log &
 tail -F /mnt/wslg/{weston,wlog}.log &
 exec "$@"
